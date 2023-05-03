d5a76df3b28e886c2362c49674d460ee898599cb
constantinus345 <96290754+constantinus345@users.noreply.github.com>
2023-05-03 19:16:49 +0300

    abc

--- curs_1.py
+++ curs_1.py
@@ -23,41 +23,42 @@
 
 #https://github.com/constantinus345/sda_48
 
-a = 18
-b = [1,2,3,7,0,"sf",6]
+
+
 
 # def impartire(a,b):
+#     if b == 0:
+#         raise ZeroDivisionError("Eroarea este divizarea la zero")
+#         #raise ne ofera posibililitatea sa facem un mesaj special al erorii, nu sa o gestionam in sine
+#
 #     try:
 #         rez = round(a/b,2)
 #         return rez
-#     except ZeroDivisionError:
-#         raise "Eroarea este ZeroDivisionError"
 #     except TypeError:
 #         print("Eroarea este TypeError")
 #     except Exception as e:
 #         print(f"Eroarea este = {e}")
-#     # except:
-#     #     print("A fost o oarecare exceptie")
-#     #CODE SMELL!
-#     # finally:
-#     #     print("Operatiunea a avut loc")
-#     #finally executa cod indiferent ca precedentele instructiuni au intrat in try sau except
-#     #= no matter what
+a = 18
+b = [1,2,3,7,0,"sf",6]
 
 def impartire(a,b):
-    if b == 0:
-        raise ZeroDivisionError("Eroarea este divizarea la zero")
-        #raise ne ofera posibililitatea sa facem un mesaj special al erorii, nu sa o gestionam in sine
-
+    rez = None
     try:
         rez = round(a/b,2)
-        return rez
+    except ZeroDivisionError:
+        print("Eroarea este ZeroDivisionError")
     except TypeError:
         print("Eroarea este TypeError")
     except Exception as e:
         print(f"Eroarea este = {e}")
-
-
+    # except:
+    #     print("A fost o oarecare exceptie")
+    #CODE SMELL!
+    # finally:
+    #     print("Operatiunea a avut loc")
+#     #finally executa cod indiferent ca precedentele instructiuni au intrat in try sau except
+#     #= no matter what
+    return rez
 
 for el in b:
     rezultatul = impartire(a,el)
